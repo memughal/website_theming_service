d9934ec8678ecd4acdca71684d46f81c57b206ca
Memoona <mem.mughal@gmail.com>
2016-08-25 16:17:27 +0200

    Added compiling css task as a job

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -1,21 +1,6 @@
 require 'less'
 class StylesheetsController < ApplicationController
   def new
-    timestamp = Time.now.to_i
-    filename = "custom_css_#{timestamp}.css"
-
-    user_variables = ""
-    user_variables << "@link-color:#009999;"
-
-    if !user_variables.empty?
-
-      parser = Less::Parser.new(paths: [Rails.root.join('vendor', 'assets', 'stylesheets', 'less')])
-      less = ('@import \'bootstrap.less\';' + user_variables)
-      tree = parser.parse(less)
-      content = tree.to_css(:compress => true)
-
-      path = Rails.root.join('public', filename)
-      File.open(path, 'w') { |file| file.write(content) }
-    end
+    CompilerJob.compile
   end
 end
