c5df738f06ade371683d6be0ced761d805fb3320
Memoona <mem.mughal@gmail.com>
2016-08-25 17:10:37 +0200

    Generate stylesheet only if model is validated else show errors

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -8,17 +8,21 @@ class StylesheetsController < ApplicationController
   def create
     @stylesheet = Stylesheet.new(stylesheet_params)
 
-    timestamp = Time.now.to_i
-    filename = "custom_css_#{timestamp}.css"
+    if @stylesheet.valid?
+      timestamp = Time.now.to_i
+      filename = "custom_css_#{timestamp}.css"
 
-    CompilerJob.delay.compile(@stylesheet, filename)
+      CompilerJob.delay.compile(@stylesheet, filename)
 
-    url = request.base_url + '/' + filename
-    flash[:url] = url
-    redirect_to action: :new
+      url = request.base_url + '/' + filename
+      flash[:url] = url
+      redirect_to action: :new
+    else
+      render :new
+    end
   end
 
   def stylesheet_params
-    params.require(:stylesheet).permit(:link_color)
+    params.require(:stylesheet).permit(:text_color, :link_color, :font_family)
   end
 end
