d742e070ab11be8ab39da8ad1772265e342a2be3
Memoona <mem.mughal@gmail.com>
2016-08-25 17:00:02 +0200

    Created new object in new and created stylesheet in the create action

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -1,13 +1,24 @@
 require 'less'
 class StylesheetsController < ApplicationController
+
   def new
+    @stylesheet = Stylesheet.new
+  end
+
+  def create
+    @stylesheet = Stylesheet.new(stylesheet_params)
 
     timestamp = Time.now.to_i
     filename = "custom_css_#{timestamp}.css"
 
-    CompilerJob.delay.compile(filename)
+    CompilerJob.delay.compile(@stylesheet, filename)
 
     url = request.base_url + '/' + filename
     flash[:url] = url
+    redirect_to action: :new
+  end
+
+  def stylesheet_params
+    params.require(:stylesheet).permit(:link_color)
   end
 end
