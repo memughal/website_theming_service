b3b5bc8a60c262601e9216ad8238c5ff50615ec3
Memoona <mem.mughal@gmail.com>
2016-08-25 17:10:14 +0200

    Added validation to the model and form i.e. atleast one property must be present to submit the form

--- app/models/stylesheet.rb
+++ app/models/stylesheet.rb
@@ -1,4 +1,14 @@
 class Stylesheet
   include ActiveModel::Model
   attr_accessor :text_color, :link_color, :font_family
+
+  validate :at_least_one_property
+
+  private
+  def at_least_one_property
+    if (text_color.blank? && link_color.blank? && font_family.blank?)
+          errors.add(:base, "Please add atleast one property")
+    end
+  end
+
 end
